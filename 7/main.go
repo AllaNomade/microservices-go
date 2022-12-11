6129cfdf4e524c303c0e6e0eb07213ef9862b85c
Alan Paiva <allanpaiva21@outlook.com>
2022-12-10 21:59:45 -0300

    functions

--- 7/main.go
+++ 7/main.go
@@ -1,5 +1,31 @@
 package main
 
+import "fmt"
+
+//map funções
 func main() {
+	salario := map[string]int{"Fulano": 100, "joao": 200}
+	fmt.Println(salario["Fulano"])
+
+	//deleta usuário
+	// // delete(salario, "Fulano")
+
+	//Add usuário
+	salario["ciclano"] = 7000
+
+	// //fmt.Println(salario)
+
+	// Em map você escolhe o tipo da chave (string boolean ou int)
+	// // salario1 := map[bool]string{true: "terra-redonda", false: "terra-plana"}
+	// // fmt.Println(salario1)
+
+	//percorrer o Maps dos salários
+	for nome, salarios := range salario {
+		fmt.Printf("O salario de %s é %d\n", nome, salarios)
+	}
+
+	for _, salarios := range salario {
+		fmt.Printf("O salario é %d\n", salarios)
+	}
 
 }
---
+++ 8/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+
+	valor, err := erro(30, 10)
+
+	if err != nil {
+		fmt.Print(err)
+	}
+	fmt.Println(valor)
+
+	fmt.Println(soma(1, 2))
+	fmt.Println(soma1(1, 2))
+}
+
+func soma(a, b int) int {
+	return a / b
+	// retorna 0 pois é um inteiro (int)
+}
+
+func soma1(a, b float64) float64 {
+	return a / b
+	// retorna 0.5 pois traz o resultado decimal da soma utilizando o float64
+}
+
+func erro(a, b int) (int, error) {
+	if a+b >= 50 {
+		return 0, errors.New("=> A soma é maior que 50: ")
+	}
+
+	return a + b, nil
+}
