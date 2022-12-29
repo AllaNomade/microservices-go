2a2b856540ed9e8206855dfc1291d231f1598d70
Alan Paiva <allanpaiva21@outlook.com>
2022-12-29 19:25:03 -0300

    feat: comments added

--- Pacotes/6templates/templateMust/main.go
+++ Pacotes/6templates/templateMust/main.go
@@ -18,13 +18,14 @@ type Curso struct {
 type Cursos []Curso
 
 func main() {
-
+	// A função t já possui um tratamento de erro, template.New conseguimos fazer a busca do arquivo
+	//Parse.Files faz leitura do arquivo com o os.Stdout setando no terminal.
 	t := template.Must(template.New("template.html").ParseFiles("template.html"))
-	// templa := template.New("CursoGotemplate")
-	// templa, err := templa.Parse("Estudo, {{.Nome}} - Carga Horária: {{.CargaHoraria}}")
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// // templa := template.New("CursoGotemplate")
+	// // templa, err := templa.Parse("Estudo, {{.Nome}} - Carga Horária: {{.CargaHoraria}}")
+	// // if err != nil {
+	// // 	panic(err)
+	// // }
 
 	err := t.Execute(os.Stdout, Cursos{
 		{"Go:", 40},
