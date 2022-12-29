511e301504c225a18a4e76272a1b35ebb9a24bc9
Alan Paiva <allanpaiva21@outlook.com>
2022-12-29 17:53:11 -0300

    feat: file template.html added

--- Pacotes/6templates/templateMust/main.go
+++ Pacotes/6templates/templateMust/main.go
@@ -10,17 +10,27 @@ type Estudo struct {
 	CargaHoraria int
 }
 
+type Curso struct {
+	Nome         string
+	CargaHoraria int
+}
+
+type Cursos []Curso
+
 func main() {
 
-	curso := Estudo{"go", 40}
-	t := template.Must(template.New("CursoTemplate").Parse("Estudo, {{.Nome}} - Carga Horária: {{.CargaHoraria}}"))
+	t := template.Must(template.New("template.html").ParseFiles("template.html"))
 	// templa := template.New("CursoGotemplate")
 	// templa, err := templa.Parse("Estudo, {{.Nome}} - Carga Horária: {{.CargaHoraria}}")
 	// if err != nil {
 	// 	panic(err)
 	// }
 
-	err := t.Execute(os.Stdout, curso)
+	err := t.Execute(os.Stdout, Cursos{
+		{"Go:", 40},
+		{"Node:", 70},
+		{"Typescript:", 50},
+	})
 	if err != nil {
 		panic(err)
 	}
