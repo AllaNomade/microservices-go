5922172726fb8cbf118ad44650369d1e846a5640
Allan <allanpaiva17@gmail.com>
2023-01-10 12:09:16 -0300

    feat:request api added

--- 3-Context/2ctxClientServer/client/main.go
+++ 3-Context/2ctxClientServer/client/main.go
@@ -1,24 +1,15 @@
 package main
 
 import (
-	"context"
-	"io"
 	"net/http"
-	"os"
-	"time"
 )
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
-	defer cancel()
-	req, err := http.NewRequestWithContext(ctx, "GET", "http://localhost:8080", nil)
-	if err != nil {
-		panic(err)
-	}
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-	io.Copy(os.Stdout, resp.Body)
+	http.HandleFunc("/", Handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
 }
