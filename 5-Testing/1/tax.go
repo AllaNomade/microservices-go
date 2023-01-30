0309a495631f2e0c5b1d6be244d878649d6cd289
Allan <allanpaiva17@gmail.com>
2023-01-30 12:30:35 -0300

    feat: fuzzing and benchmarking test examples

--- 5-Testing/1/tax.go
+++ 5-Testing/1/tax.go
@@ -1,6 +1,24 @@
 package tax
 
+import (
+	"time"
+)
+
 func Calculate(amount float64) float64 {
+	if amount <= 0 {
+		return 0
+	}
+	if amount >= 1000 && amount < 20000 {
+		return 10.0
+	}
+	if amount >= 20000 {
+		return 20.0
+	}
+	return 5.0
+}
+
+func Calculate2(amount float64) float64 {
+	time.Sleep(time.Millisecond * 700)
 	if amount == 0 {
 		return 0
 	}
