a5d6c86e4e05e44f8fd7de6c59585e009b01d555
Alan Paiva <allanpaiva21@outlook.com>
2023-01-31 21:55:58 -0300

    feat: code tested with 100% of covered

--- 5-Testing/3/car.go
+++ 5-Testing/3/car.go
@@ -10,11 +10,11 @@ func SavePriceCar(value float64, repository Repository) error {
 }
 
 func CarPrice(value float64) int {
-	if value <= 20000 {
-		return 20000.0
+	if value == 20000 {
+		return 20000
 	}
 	if value == 55000 {
-		return 55000.0
+		return 55000
 	}
 	return 10000
 }
--- 5-Testing/3/car_test.go
+++ 5-Testing/3/car_test.go
@@ -11,6 +11,7 @@ func TestCarValue(t *testing.T) {
 	repository := &ValueRepositoryMock{}
 	repository.On("ValueCar", 20000).Return(errors.New("error ins show value"))
 	repository.On("ValueCar", 55000).Return(errors.New("error ins show value"))
+	repository.On("ValueCar", 10000).Return(nil)
 
 	err := SavePriceCar(20000.0, repository)
 	assert.Error(t, err, "error in show value")
@@ -18,5 +19,19 @@ func TestCarValue(t *testing.T) {
 	err = SavePriceCar(55000.0, repository)
 	assert.Error(t, err, "error in show value")
 
+	err = SavePriceCar(10000, repository)
+	assert.Nil(t, err)
+
 	repository.AssertExpectations(t)
+	repository.AssertNumberOfCalls(t, "ValueCar", 3)
+}
+
+func TestPrice(t *testing.T) {
+	value := 500
+	expected := 10000
+
+	result := CarPrice(float64(value))
+	if result != expected {
+		t.Error(expected, result)
+	}
 }
